afeb1ff872ccde829fda4e52b9efa06b7cecbbf9
lanefaison2 <faisonusmc@gmail.com>
2017-03-15 00:04:36 -0600

    css, recipe page

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,29 +1,6 @@
 $(document).ready(function () {
-  var ctx = document.querySelector("canvas").getContext("2d"),
-    dashLen = 220, dashOffset = dashLen, speed = 10,
-    txt = "DRINK   MENU", x = 30, i = 0;
 
-  ctx.font = "50px Berkshire Swash, cursive, TSCu_Comic, sans-serif";
-  ctx.lineWidth = 2; ctx.lineJoin = "round"; ctx.globalAlpha = 0.9;
-  ctx.strokeStyle = ctx.fillStyle = "#e4a82f";
-
-  (function loop() {
-    ctx.clearRect(x, 0, 60, 150);
-    ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask
-    dashOffset -= speed;                                         // reduce dash length
-    ctx.strokeText(txt[i], x, 90);                               // stroke letter
-
-    if (dashOffset > 0) requestAnimationFrame(loop);             // animate
-    else {
-      ctx.fillText(txt[i], x, 90);                               // fill final letter
-      dashOffset = dashLen;                                      // prep next char
-      x += ctx.measureText(txt[i++]).width + ctx.lineWidth * Math.random();
-      ctx.setTransform(1, 0, 0, 1, 0, 3 * Math.random());        // random y-delta
-      ctx.rotate(Math.random() * 0.005);                         // random rotation
-      if (i < txt.length) requestAnimationFrame(loop);
-    }
-  })();
-
+  // Shaker animation
   $('.drink-logo-div').hover(function () {
 
     if (!$('.drink-logo-div').hasClass('shaking')) {
@@ -43,4 +20,33 @@ $(document).ready(function () {
       })
     }
   })
+  // End of shaker animation
+
+  // Calling the function that writes 'Drink Menu'
+  var ctx = document.querySelector("canvas").getContext("2d"),
+  dashLen = 220, dashOffset = dashLen, speed = 10,
+  txt = "DRINK   MENU", x = 30, i = 0;
+
+  ctx.font = "50px Berkshire Swash, cursive, TSCu_Comic, sans-serif";
+  ctx.lineWidth = 2; ctx.lineJoin = "round"; ctx.globalAlpha = 0.9;
+  ctx.strokeStyle = ctx.fillStyle = "#e4a82f";
+  // End of 'Drink Menu' call
 })
+
+// Function that writes 'Drink Menu'
+(function loop() {
+  ctx.clearRect(x, 0, 60, 150);
+  ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask
+  dashOffset -= speed;                                         // reduce dash length
+  ctx.strokeText(txt[i], x, 90);                               // stroke letter
+
+  if (dashOffset > 0) requestAnimationFrame(loop);             // animate
+  else {
+    ctx.fillText(txt[i], x, 90);                               // fill final letter
+    dashOffset = dashLen;                                      // prep next char
+    x += ctx.measureText(txt[i++]).width + ctx.lineWidth * Math.random();
+    ctx.setTransform(1, 0, 0, 1, 0, 3 * Math.random());        // random y-delta
+    ctx.rotate(Math.random() * 0.005);                         // random rotation
+    if (i < txt.length) requestAnimationFrame(loop);
+  }
+})();
