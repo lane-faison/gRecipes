2bfd4fb559957636efc7d0cbe151a449877a05d1
lanefaison2 <faisonusmc@gmail.com>
2017-03-20 00:21:27 -0600

    ingredient page jquery updates"

--- public/assets/scripts/ingredient.js
+++ public/assets/scripts/ingredient.js
@@ -5,15 +5,38 @@ var server = heroku
 $(document).ready(function () {
   $.get(`${server}/ingredients`, function (data) {
     data.forEach(function (ingredient, i, data) {
-      $('.ingredient-list').append(`<h4 id=${ingredient.id} class='ingredient-item'>${ingredient.name}</h4>`)
+      $('.ingredient-list').append(`<div class='ingredient-item-div'>
+      <button type='button' id=${ingredient.id} class='btn-ingredient'>
+      <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+      <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+      </button>
+      <h4 class='ingredient-item'>${ingredient.name}</h4></div>`)
     })
   })
 })
 
-$(document).on('click','.ingredient-item', function (event) {
-  $(event.target).addClass('chosen')
+$(document).on('click','.btn-ingredient', function () {
+
   $('.available-drink').remove()
-  var selectedID = $(event.target).attr('id')
+
+  if (!$(this).hasClass('gotIt')) {
+    $('.btn-ingredient').removeClass('gotIt')
+    $(this).find('.glyphicon-plus').hide()
+    $(this).find('.glyphicon-ok').show()
+    $(this).addClass('gotIt')
+  }
+
+  else {
+    $(this).find('.glyphicon-plus').show()
+    $(this).find('.glyphicon-ok').hide()
+    $(this).removeClass('gotIt')
+  }
+
+  // TODO: REMOVE ACTUAL CHECK AFTER A DIFFERENT CLICK. CURRENTLY THE COLORS CHANGE PROPERLY BUT THE CHECK GLYPHICON REMAINS.
+
+  var selectedID = $(this).attr('id')
+  console.log(selectedID)
+
   $.get(`${server}/join`, function (data) {
     var recipesNeeded = []
     for (var i = 0; i < data.length; i++) {
@@ -21,16 +44,14 @@ $(document).on('click','.ingredient-item', function (event) {
         recipesNeeded.push(data[i].recipe_id)
       }
     }
-    console.log('recipesNeeded')
-    console.log(recipesNeeded)
     return Promise.all(recipesNeeded)
     .then(function (result) {
-      console.log(result);
       $.get(`${server}/recipes`, function (data) {
         for (var j = 0; j < result.length; j++) {
           for (var k = 0; k < data.length; k++) {
             if (data[k].id == result[j]) {
-              $('.drink-list').append(`<h4 class='available-drink'>${data[k].name}</h4>`)
+              console.log(data[k].id);
+              $('.drink-list').append(`<div class='available-item-div'><h4 class='available-drink'>${data[k].name}</h4></div>`)
             }
           }
         }
@@ -38,5 +59,3 @@ $(document).on('click','.ingredient-item', function (event) {
     })
   })
 })
-
-// $('.available-drinks').append(`<h4>`)
