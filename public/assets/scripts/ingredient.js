2026d84032fc9cdfda84adfb1a04dc6d2612a90d
lanefaison2 <faisonusmc@gmail.com>
2017-03-19 19:55:19 -0600

    ingredient page update

--- public/assets/scripts/ingredient.js
+++ public/assets/scripts/ingredient.js
@@ -5,17 +5,36 @@ var server = heroku
 $(document).ready(function () {
   $.get(`${server}/ingredients`, function (data) {
     data.forEach(function (ingredient, i, data) {
-      console.log(ingredient);
       $('.ingredient-list').append(`<h4 id=${ingredient.id} class='ingredient-item'>${ingredient.name}</h4>`)
     })
   })
 })
 
 $(document).on('click','.ingredient-item', function (event) {
-  $(this).addClass('chosen')
-  var selectedID = $(this).attr('id')
+  $(event.target).addClass('chosen')
+  var selectedID = $(event.target).attr('id')
   console.log(selectedID)
   $.get(`${server}/join`, function (data) {
     console.log(data);
+    var recipesNeeded = []
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].ingredient_id == selectedID) {
+        recipesNeeded.push(data[i].recipe_id)
+      }
+    }
+    return Promise.all(recipesNeeded)
+    .then(function (result) {
+      $.get(`${server}/recipes`, function (data) {
+        for (var i = 0; i < data.length; i++) {
+          for (var k = 0; k < result.length; k++) {
+            if (array[i].name == result[k]) {
+              $('.drink-list').append(`<h4>${data[i].name}</h4>`)
+            }
+          }
+        }
+      })
+    })
   })
 })
+
+// $('.available-drinks').append(`<h4>`)
